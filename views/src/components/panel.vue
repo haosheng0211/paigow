20c3a7979e3132fbe88744fec89dc3ca7e548c7c
MrJin <haosheng0211@gmail.com>
2021-10-10 04:56:51 +0800

    feat: 新增 Panel 組件

---
+++ views/src/components/panel.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-card class="plane">
+    <v-card-title
+      v-if="$slots.actions"
+      class="panel-actions"
+    >
+      <slot name="actions" />
+    </v-card-title>
+    <v-card-title
+      v-if="$slots.filters"
+      class="panel-filters"
+    >
+      <slot name="filters" />
+    </v-card-title>
+    <slot name="default" />
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: 'Panel'
+}
+</script>
+
+<style lang="scss">
+.panel-actions {
+  .v-btn:not(:last-child) {
+    margin-right: 8px;
+  }
+}
+</style>
