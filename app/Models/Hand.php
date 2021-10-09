5ce9c84d3dd5558bfa9a35f67a17795f12392ade
MrJin <haosheng0211@gmail.com>
2021-10-10 02:07:16 +0800

    feat: 修改 Hand 數據類型

--- app/Models/Hand.php
+++ app/Models/Hand.php
@@ -10,10 +10,10 @@ use Illuminate\Database\Eloquent\Model;
 /**
  * App\Models\Hand
  *
- * @property int    $id
- * @property int    $patterns_id
- * @property string $cards
- * @property int    $order
+ * @property int   $id
+ * @property int   $patterns_id
+ * @property array $cards
+ * @property int   $order
  * @method static Builder|Hand newModelQuery()
  * @method static Builder|Hand newQuery()
  * @method static Builder|Hand query()
@@ -28,4 +28,8 @@ class Hand extends Model
     use HasFactory;
 
     public $timestamps = false;
+
+    protected $casts = [
+        'cards' => 'array'
+    ];
 }
