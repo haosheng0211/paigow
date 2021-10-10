cea555f652209e5b5b27d0222552ee6ac5b4dbdb
MrJin <haosheng0211@gmail.com>
2021-10-11 00:06:58 +0800

    feat: 新增 Order,Pairs Select 組件

---
+++ views/src/components/select/order-select.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-select
+    v-model="model"
+    v-bind="$attrs"
+    :items="items"
+  />
+</template>
+
+<script>
+import ValueMixin from '@/utils/mixins/value-mixin'
+export default {
+  name: 'OrderSelect',
+  mixins: [ValueMixin],
+  data () {
+    return {
+      items: [
+        { text: '南部比分高到低', value: 'score_south,desc' },
+        { text: '南部比分低到高', value: 'score_south,asc' },
+        { text: '北部比分高到低', value: 'score_north,desc' },
+        { text: '北部比分低到高', value: 'score_north,asc' },
+        { text: '排序權重高到低', value: 'order,asc' },
+        { text: '排序權重低到高', value: 'order,desc' }
+      ]
+    }
+  }
+}
+</script>
---
+++ views/src/components/select/pairs-select.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-select
+    v-model="model"
+    v-bind="$attrs"
+    :items="items"
+  />
+</template>
+
+<script>
+import ValueMixin from '@/utils/mixins/value-mixin'
+export default {
+  name: 'PairsSelect',
+  mixins: [ValueMixin],
+  data () {
+    return {
+      items: [
+        { text: '對子', value: 1 },
+        { text: '其他', value: 0 }
+      ]
+    }
+  }
+}
+</script>
