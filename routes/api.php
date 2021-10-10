632de52f7195859cac621d38b1ec7dc23bd6db18
MrJin <haosheng0211@gmail.com>
2021-10-10 16:27:47 +0800

    feat: 修改 card 為 cards

--- routes/api.php
+++ routes/api.php
@@ -2,5 +2,5 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('card/options', 'CardsController@options');
+Route::get('cards/options', 'CardsController@options');
 Route::apiResource('composes', 'ComposesController')->only(['index', 'update']);
