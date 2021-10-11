3dbcef174525099a56c448ecf2963bf35460c7a7
MrJin <haosheng0211@gmail.com>
2021-10-11 23:37:30 +0800

    feat: 新增 Seeder 數據填充

--- routes/api.php
+++ routes/api.php
@@ -3,4 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('cards/options', 'CardsController@options');
+Route::post('compute', 'ComputeController');
+Route::post('composes/export', 'ComposesController@export');
+Route::post('composes/import', 'ComposesController@import');
 Route::apiResource('composes', 'ComposesController')->only(['index', 'update']);
+Route::delete('records', 'RecordsController@destroy');
+Route::apiResource('records', 'RecordsController')->only(['index']);
