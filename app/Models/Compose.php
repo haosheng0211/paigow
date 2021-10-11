38a0bc5ec5cfe91daf4eed615db7f38acd4b5fe5
MrJin <haosheng0211@gmail.com>
2021-10-11 19:40:32 +0800

    feat: 新增 Composes 分析方法

--- app/Models/Compose.php
+++ app/Models/Compose.php
@@ -6,6 +6,7 @@ use Eloquent;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Arr;
 
 /**
  * App\Models\Compose
@@ -15,8 +16,8 @@ use Illuminate\Database\Eloquent\Model;
  * @property array       $cards
  * @property int         $pairs
  * @property int         $order
- * @property string      $score_south
- * @property string      $score_north
+ * @property float       $score_south
+ * @property float       $score_north
  * @method static Builder|Compose newModelQuery()
  * @method static Builder|Compose newQuery()
  * @method static Builder|Compose query()
@@ -36,6 +37,32 @@ class Compose extends Model
     public $timestamps = false;
 
     public $casts = [
-        'cards' => 'array'
+        'cards'       => 'array',
+        'score_north' => 'float',
+        'score_south' => 'float',
     ];
+
+    public static function analyze(array $values, $location = 'north'): ?array
+    {
+        $values = Arr::sort($values);
+
+        $composes = self::select(['label', 'cards', "score_{$location}"])
+            ->orderBy("score_{$location}", 'desc')
+            ->orderBy('order')
+            ->get();
+
+        $composes = $composes->map(function (Compose $compose) use (&$values, $location) {
+            if (count($cards = array_intersect($values, $compose->cards)) !== 2) {
+                return null;
+            }
+
+            foreach ($cards as $key => $val) {
+                unset($values[$key]);
+            }
+
+            return $compose;
+        })->toArray();
+
+        return array_values(array_filter($composes));
+    }
 }
